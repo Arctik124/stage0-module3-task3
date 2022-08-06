e23b23e992ac960918d3faa15873f9e8358df953
Artyom <artvit12@gmail.com>
2022-08-06 11:43:28 +0600

    Solution

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,5 +3,11 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
+        //Declare 3 int variables within one statement and call them a, b, c (order must be followed).
+        //Initialize them in one statement with value "10". Print to console "c".
+        //Implement the program inside of given code snippet:
+        int a, b, c;
+        a = b = c = 10;
+        System.out.println(c);
     }
 }
